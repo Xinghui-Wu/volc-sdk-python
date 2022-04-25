b7313e5d0ea7f2b989a07c464e9a2abb48c730a7
lvwenhua <lvwenhua@bytedance.com>
2022-04-25 11:08:27 +0800

    feat: live_add_sdk_fixbug

--- volcengine/example/live/example_describe_info/example_describe_info.py
+++ volcengine/example/live/example_describe_info/example_describe_info.py
@@ -186,8 +186,8 @@ def example_describe_play_stream_list(live_service):
 
 if __name__ == '__main__':
     live_service = LiveService()
-    ak = "AKLTZDkyYTQyYmE3NzBmNDE4OWJmYTllYzY0ODhmN2MwZTI"
-    sk = "TkRkak5tUTFOalJpWkRVMU5EWTRZVGd4TmpsaE1tRmhOVEEwTXpKaFlUWQ=="
+    ak = ""
+    sk = "=="
     live_service.set_ak(ak)
     live_service.set_sk(sk)
     # 1.查询直播域名带宽数据
